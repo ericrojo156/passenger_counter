f12c8ed9c2b59002aa86d2e1fe94593558b351aa
Eric Rojo <ericrojo156@hotmail.com>
2020-05-31 15:57:28 -0700

    changed direction of divider line to be vector instead of angle

--- device/DividerLine.py
+++ device/DividerLine.py
@@ -6,15 +6,15 @@ class DividerLine:
         if (len(serialized) == 0):
             raise Exception("Failed to load serialized data for the divider line from the device configuration file.")
         line_dict = json.loads(serialized)
-        self.onboarding_direction_degrees = self.getOnBoardingDirection(line_dict)
+        self.onboarding_direction_vector = self.getOnBoardingDirection(line_dict)
         line_equation = self.getLineEquation(line_dict)
         self.slope = line_equation.get("a", 1)
         self.intercept = line_equation.get("b", 0)
         self.order = line_equation.get("order", 1)
 
     def getOnBoardingDirection(self, line_dict):
-        directionAngleDegrees = line_dict.get("onBoardingDirection", 0)
-        return directionAngleDegrees
+        onboarding_direction_vector = line_dict.get("onBoardingDirection", [1, 1])
+        return onboarding_direction_vector
 
     def getLineEquation(self, line_dict):
         return line_dict.get("equation")
