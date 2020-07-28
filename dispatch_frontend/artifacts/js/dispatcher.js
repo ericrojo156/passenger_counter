700d6db9fd5d554c30b319be637eaa98f99d3722
Eric Rojo <ericrojo156@hotmail.com>
2020-07-27 18:21:00 -0700

    removed artifacts folder

--- dispatch_frontend/artifacts/js/dispatcher.js
+++
@@ -1,3 +0,0 @@
-import { Dispatcher } from 'flux';
-export default Dispatcher;
-//# sourceMappingURL=dispatcher.js.map
--- dispatch_frontend/artifacts/js/redux/Reducers.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-const State = combineReducers({});
-export default State;
-//# sourceMappingURL=Reducers.js.map
